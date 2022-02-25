2e1ce8f3b35400cf35528492ce6a6147372a365b
Arian <arianrdcas@gmail.com>
2022-02-25 10:12:08 -0300

    listo

--- src/App.js
+++ src/App.js
@@ -1,40 +1,62 @@
-import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+import { ListGroup } from 'react-bootstrap';
+
+import './scss/app.scss';
 import Menu from './Menu';
 
-
 function App() {
+
+  const[lista, setLista] = useState([]);
+  
+  const[songurl, setSongurl] = useState("");
+  const[songid, setSongid] = useState("");
+
+  useEffect(() => {
+		getList();
+	}, []);
+
+  const getList = async () => {
+		const data = await fetch('https://assets.breatheco.de/apis/sound/songs',{
+      'method':'GET',
+      headers : {
+          'Content-Type':'application/json'
+      }, 
+    })
+		const cancion = await data.json();
+		setLista(cancion);
+	};
+
   return (
     <>
-      <h1 className="titulo">Music Player con React</h1>
-      <div className="container">
-        <div >
-          <ol className="lista">
-            <li>Mario Castle</li>
-            <hr/>
-            <li>Mario Star</li>
-            <hr/>
-            <li>Mario Overworld</li>
-            <hr/>
-            <li>Mario Stage 1</li>
-            <hr/>
-            <li>Mario Stage 2</li>
-            <hr/>
-            <li>Mario Star-2</li>
-            <hr/>
-            <li>Mario Underworld</li>
-            <hr/>
-            <li>Mario Underwater</li>
-            <hr/>
-            <li>Zelda Castle</li>
-            <hr/>
-            <li>Zelda Outworld</li>
-            <hr/>
-            <li>Zelda Titles</li>
-            <hr/>
-          </ol>
-        </div>
-        <Menu />
-      </div>
+        <h1 className="titulo">Music Player con React</h1>
+
+          <ListGroup as="ol" numbered className="ol">
+          {
+            lista.map( cancion => (
+              <ListGroup.Item as="li" 
+              onClick= { e => {
+                //console.log("test");
+                //console.log(cancion.url);
+                setSongurl(cancion.url)
+                setSongid(cancion.id)
+                }
+              }>
+                
+                {cancion.name} 
+                
+              </ListGroup.Item>
+                
+            ))}
+          </ListGroup>
+
+          <Menu 
+              url = {songurl}
+              id = {songid}
+              canciones = {lista}
+          />
+      
     </>
   );
 }
--- src/Menu.js
+++ src/Menu.js
@@ -1,28 +1,94 @@
 import React from "react";
+import './App.css';
 import { useState } from "react";
+import {AiFillFastBackward, AiFillFastForward, AiFillPlayCircle, AiFillPauseCircle} from "react-icons/ai";
+import {Howl} from "howler";
+import { Container, Button  } from "react-bootstrap";
 
 
 
-const Menu = () => {
+const Menu = (props) => {
 
-    const[reproducir, setReproducir]= useState("")
+    const url = "https://assets.breatheco.de/apis/sound/";
 
-    const Play = (reproducir) =>{
-        return(
-            console.log("Haciendo Cilck")
-        )
-        
+    const[estado, setEstado] = useState(false);
+
+    const[musica, setMusica]= useState("");
+
+    const[idsound, setIdsound] = useState(null);
+
+    const sounplay = (src, ids) =>{
+        const sound = new Howl({
+            src,
+            html5:true
+        })
+
+        //console.log(sound);
+
+        setMusica(sound);
+
+        setIdsound(ids);
+
+        setEstado(true);
+        sound.play();
     }
 
+    const sounstop = () => {
+        //console.log("stop")
+        musica.stop();
+        setEstado(false);
+	};
+
+    const sounback = (listacanciones) =>{
+        sounstop();
+        const idback = idsound - 1;
+        const a = listacanciones.find(p => p.id === idback).url;
+        
+        const  src = url+ a
+        const sound = new Howl({
+            src,
+            html5:true
+        })
+
+        setMusica(sound);
+
+        setEstado(true);
+        sound.play();
+        setIdsound(idback);
+    };
+
+    const sounnext = (listacanciones) =>{
+        sounstop();
+        const idnext = idsound + 1;
+        const a = listacanciones.find(p => p.id === idnext).url;
+        
+        const  src = url+ a
+        const sound = new Howl({
+            src,
+            html5:true
+        })
+
+        setMusica(sound);
+
+        setEstado(true);
+        sound.play();
+        setIdsound(idnext);
+    };
 
     return ( 
-        <div className="controles" >
-            <button type="button">A</button>
-            <button type="button" onClick={Play}>P</button>
-            <button type="button">N</button>
+        <div className="menu d-flex justify-content-evenly bg-dark fixed-bottom">
+            <Button className="btn btn-dark  position-relative fs-3" id = "back" type="button" onClick={e => {sounback(props.canciones);}}><AiFillFastBackward/></Button >
+            
+            { !estado? 
+
+                <Button className="btn btn-dark  position-relative fs-3" id = "play" type="button" onClick={e => {sounplay(url+props.url, props.id);}}>< AiFillPlayCircle/></Button > : <Button className="btn btn-dark position-relative fs-3" id = "pausa" type="button"  onClick={e => {sounstop(url+props.url);}}><AiFillPauseCircle/> </Button> 
+            }
+
+            <Button className="btn btn-dark  position-relative fs-3" id = "next" type="button" onClick={e => {sounnext(props.canciones);}}><AiFillFastForward/></Button >
         </div>
 
     );
 }
 
 export default Menu;
+
